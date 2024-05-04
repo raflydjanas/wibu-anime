d98b68b6e69efe0c9fa40dfdb5e70011d9d54be3
raflydjanas <moh.raflidjans@gmail.com>
2024-05-05 00:30:55 +0800

    create env file for url api

--- app/actions.tsx
+++ app/actions.tsx
@@ -2,7 +2,7 @@
 import AnimeCard, { AnimeProps } from "@/components/AnimeCard";
 
 export const fecheAnimes = async (page: number) => {
-  const res = await fetch(`https://shikimori.one/api/animes?${page}&limit=8&order=popularity`);
+  const res = await fetch(`${process.env.URL_API}${page}&limit=8&order=popularity`);
   const data = await res.json();
 
   return data.map((anime: AnimeProps, index: number) => <AnimeCard key={anime.id} anime={anime} index={index} />);
